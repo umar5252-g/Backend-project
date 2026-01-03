e8688048f245df6b786e5a19a15a3d27b457b705
umar5252-g <mromertariq@gmail.com>
2026-01-03 18:38:38 +0500

    install dotenv and used it

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -9,7 +10,16 @@ app.get("/", (req, res) => {
 app.get("/twitter", (req, res) => {
   res.send("UmarKhan-5252");
 });
+app.get("/insta", (req, res) => {
+  res.send("UmarKhan");
+});
+app.get("/login", (req, res) => {
+  res.send("<h1>Please enter your login information</h1>");
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/youtube", (req, res) => {
+  res.send("<h1>chai aur backend</h1>");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
