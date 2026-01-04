7f091c596c03364eaadd97a92825bfaf0ca5d447
umar5252-g <mromertariq@gmail.com>
2026-01-04 21:45:55 +0500

    connect frontend with backend

---
+++ fullstack-basic/backend/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+
+const app = express();
+
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "a 5th joke",
+      content: "this is a first joke",
+    },
+    {
+      id: "2",
+      title: "a 2nd joke",
+      content: "this is a second joke",
+    },
+    {
+      id: 3,
+      title: "a th3 joke",
+      content: "this is a third joke",
+    },
+    {
+      id: 4,
+      title: "a 4th joke",
+      content: "this is a third joke",
+    },
+    {
+      id: 5,
+      title: "a 5th joke",
+      content: "this is a third joke",
+    },
+  ];
+  res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`serve at http:/localhost:${port} `);
+});
---
+++ fullstack-basic/frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
+import { useEffect } from "react";
+
+function App() {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/jokes")
+      .then((response) => {
+        setJokes(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <>
+      <h1>Backend learning</h1>
+      <p>JOKES : {jokes.length}</p>
+      {jokes.map((joke) => (
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default App;
