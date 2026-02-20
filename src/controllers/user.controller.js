04326197320c616a64d886f4c501de65e0d055ee
umar5252-g <mromertariq@gmail.com>
2026-02-20 21:41:36 +0500

    check is user already exist before registring

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,33 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { apiError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "chai or code",
+  //get user details from frontend
+  //validation - empty
+  //check if user already exists: username, email
+  //check for images, check for avatar
+  //upload them to cloudinary, avatar
+  //create user object - create entry in db
+  //remove password and refresh token fields from response
+  //check for user creation
+  //return res
+
+  const { fullName, email, username, password } = req.body;
+  console.log("email: ", email);
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new apiError(400, " All fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
   });
+  if (existedUser) {
+    throw new apiError(409, "User with the email or username already exists");
+  }
 });
 
 export { registerUser };
--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ import connectDB from "./db/index.js";
 import { app } from "./app.js";
 dotenv.config({ path: "./.env" });
 
-connectDB()
-  .then(() => {
-    app.on("error", (error) => {
-      console.log("Error: ", error);
-      throw error;
-    });
+// connectDB()
+//   .then(() => {
+//     app.on("error", (error) => {
+//       console.log("Error: ", error);
+//       throw error;
+//     });
 
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`server is running at port: ${process.env.PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.log("MongoDB connection failed:", error);
-  });
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`server is running at port: ${process.env.PORT}`);
+});
+//   })
+//   .catch((error) => {
+//     console.log("MongoDB connection failed:", error);
+//   });
 
 // (async () => {
 //   try {
