3704f629f348796d44c6bb6e1e98e4772df49e97
umar5252-g <mromertariq@gmail.com>
2026-03-07 12:06:29 +0500

    add refreshAccessToken functionality and update routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,7 @@ import {
   logoutUser,
   registerUser,
   loginUser,
+  refreshAccessToken,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -20,5 +21,7 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
